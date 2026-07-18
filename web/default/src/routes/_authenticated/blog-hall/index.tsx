67f2744d026ece28e685d1c26c13fe6049a26908
Qi Ma <mq@lanhc.com>
2026-07-18 22:33:18 +0800

    chore: update version to 1.4.35 in VERSION and version.ini files; refactor BlogHall component and add search validation

--- web/default/src/routes/_authenticated/blog-hall/index.tsx
+++ web/default/src/routes/_authenticated/blog-hall/index.tsx
@@ -17,9 +17,18 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 For commercial licensing, please contact support@quantumnous.com
 */
 import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
 
 import { BlogHall } from '@/features/blog-hall'
+import { BLOG_ARTICLE_STATUS_VALUES } from '@/features/blog-hall/constants'
+
+const blogHallSearchSchema = z.object({
+  page: z.number().optional().catch(1),
+  pageSize: z.number().optional().catch(20),
+  status: z.array(z.enum(BLOG_ARTICLE_STATUS_VALUES)).optional().catch([]),
+})
 
 export const Route = createFileRoute('/_authenticated/blog-hall/')({
+  validateSearch: blogHallSearchSchema,
   component: BlogHall,
 })
